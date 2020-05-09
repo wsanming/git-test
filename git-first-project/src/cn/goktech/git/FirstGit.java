d528aa0c24da679df4a7f6a2a7792f73ec105a0c
sanming <841657622@qq.com>
2020-05-09 14:49:49 +0800

    将项目提交到git本地仓库

---
+++ git-first-project/src/cn/goktech/git/FirstGit.java
@@ -0,0 +1,15 @@
+package cn.goktech.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+    public void test(){
+        int i=0;
+    }
+}
